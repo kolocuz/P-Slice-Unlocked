e871c6ae4b444c00a8bc479e092c77716966adf0
kolocuz <koloc232@gmail.com>
2026-07-27 23:27:04 +0300

    Delete firebase_ios.sh

--- setup/firebase_ios.sh
+++
@@ -1,4 +0,0 @@
-# Replace "export/ios/"  with the path where the android build is stored
-
-chmod +x ./export/release/ios/upload-symbols
-./export/release/ios/upload-symbols -gsp ./setup/GoogleService-Info.plist -p ios  ./export/release/ios/build/Release-iphoneos/PSliceEngine.app.dSYM
